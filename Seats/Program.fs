f0aa84295fa628909173c65de02ffe8376fad488
Timxthy <Timxthy72@gmail.com>
2022-05-13 02:02:21 +0100

    sEATS

---
+++ Seats/Program.fs
@@ -0,0 +1,14 @@
+﻿// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
+
+open System
+
+type Ticket = {seat:int; customer:string}
+
+let mutable tickets = [for n in 1..10 -> {Ticket.seat = n; Ticket.customer = ""}]
+
+let bookSeat seatNo name tickets = 
+  tickets |> List.map (fun ticket ->
+    if ticket.seat = seatNo then { ticket with customer = name }
+    else ticket )
+
+
